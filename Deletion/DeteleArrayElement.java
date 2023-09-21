7d9815486c0472421000b63525f9bd295ff3d67d
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2023-09-21 12:05:46 +0530

    Add files via upload

---
+++ Deletion/DeteleArrayElement.java
@@ -0,0 +1,18 @@
+package DSPractice.Deletion;
+
+public class DeteleArrayElement {
+    public static void main(String[] args) {
+        int[] oldArray = {1, 2, 3, 4, 5, 100};
+        int elementToDelete = 100;
+        int[] newArray = new int[oldArray.length - 1];
+        int newIndex = 0;
+        for (int i = 0; i < oldArray.length; i++) {
+            if (oldArray[i] != elementToDelete) {
+                newArray[newIndex] = oldArray[i];
+                newIndex++;
+            }
+        }
+        for (int i : newArray)
+            System.out.println(i);
+    }
+}
