97c279b1c412265c58e60bb3fa96fe9d1fcb9377
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2023-09-21 11:08:32 +0530

    Add files via upload

---
+++ Insertion/ArrayElementInsert.java
@@ -0,0 +1,19 @@
+package DSPractice.Insertion;
+
+public class ArrayElementInsert {
+    public static void main(String[] args) {
+        int[] oldArray = {2, 4, 3, 67, 45, 23};
+        int insertElement = 9;
+        int insertIndex = 4;
+        int[] newArray = new int[oldArray.length + 1];
+        for (int i = 0, j = 0; i < oldArray.length; i++, j++) {
+            if (i == insertIndex) {
+                newArray[j] = insertElement;
+                j++;
+            }
+            newArray[j] = oldArray[i];
+        }
+        for (int i : newArray)
+            System.out.println(i);
+    }
+}
---
+++ Insertion/ArrayElementInsertArrayUtils.java
@@ -0,0 +1,14 @@
+package DSPractice.Insertion;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class ArrayElementInsertArrayUtils {
+    public static void main(String[] args) {
+        int[] oldArray = {2, 4, 3, 67, 45, 23};
+        int insertElement = 9;
+        int insertIndex = 4;
+        int[] newArray = ArrayUtils.insert(insertIndex, oldArray, insertElement);
+        for (int arr : newArray)
+            System.out.println(arr);
+    }
+}
